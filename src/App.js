90dc5eac1e35f3d1b7046643c6e941bc1a75dd2d
opuzayed <zayedopu4933@gmail.com>
2022-12-05 16:53:06 +0600

    set up done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+     <RouterProvider router={router} />
     </div>
   );
 }
