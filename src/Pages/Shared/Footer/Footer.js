72f3758cfcdcbe86d4367b56784283efa3befb36
opuzayed <zayedopu4933@gmail.com>
2022-12-08 23:07:47 +0600

    year set done dynamically in the footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
+  function getYear() {
+    return new Date().getFullYear();
+  }
+  return (
+    <footer className="p-10">
+      <div className="footer">
         <div>
-            <h2>This is footer</h2>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
         </div>
-    );
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </div>
+      <div className="text-center mt-10">
+        <p>
+          Copyright <span>© {getYear()} - All right
+          reserved by ACME Industries Ltd</span> 
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
