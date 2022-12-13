6255cd9cff3407d8d28d3238f1dff4edd5731504
opuzayed <zayedopu4933@gmail.com>
2022-12-13 23:56:20 +0600

    errror handling from react hook form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, formState: { errors }, handleSubmit } = useForm();
   
   const handleLogin = data =>{
     console.log(data);
@@ -19,11 +18,13 @@ const Login = () => {
             <label className="label">
               <span className="label-text ">Email</span>
             </label>
-            <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs"/>
+            <input type="text" {...register("email", { required:"Email Address is required"})} className="input input-bordered w-full max-w-xs"/>
+            {errors.email && <p className="text-error-content">{errors.email?.message}</p>}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label"> <span className="label-text">Password</span></label>
-            <input type="text" {...register("password")} className="input input-bordered w-full max-w-xs"/>
+            <input type="text" {...register("password", { required: "Password is required", minLength: { value: 6, message: "password must be six character or longer"}})} className="input input-bordered w-full max-w-xs"/>
+            {errors.password && <p className="text-error-content">{errors.password?.message}</p>}
             <label className="label"> <span className="label-text">Forget Password?</span></label>
           </div>
           <input className="btn btn-accent w-full" value='Login' type="submit" />
