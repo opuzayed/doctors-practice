1df9652b212ae4b227141e7d1b412fa835e99000
opuzayed <zayedopu4933@gmail.com>
2022-12-13 21:00:02 +0600

    email and password field done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,21 +3,26 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Login = () => {
-    const { register, handleSubmit } = useForm();
-    const [data, setData] = useState("");
-    
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+
   return (
     <div className="h-[800px] flex justify-center items-center">
       <div>
-        <h2 className="text-5xl text-center">Login Here</h2>
+        <h2 className="text-5xl text-center my-8">Login Here</h2>
         <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-          <input {...register("firstName")} placeholder="First name" />
-          <select {...register("category", { required: true })}>
-            <option value="">Select...</option>
-            <option value="A">Option A</option>
-            <option value="B">Option B</option>
-          </select>
-          <textarea {...register("aboutYou")} placeholder="About you" />
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text text-2xl">Email</span>
+            </label>
+            <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs"/>
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text text-2xl">Password</span>
+            </label>
+            <input type="text" {...register("password")} className="input input-bordered w-full max-w-xs"/>
+          </div>
           <p>{data}</p>
           <input type="submit" />
         </form>
