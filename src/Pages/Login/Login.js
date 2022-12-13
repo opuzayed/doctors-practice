3a4d599aa5cfc9f327cea140026a823804fbf92b
opuzayed <zayedopu4933@gmail.com>
2022-12-13 21:35:05 +0600

    continue with google button done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
@@ -8,24 +9,25 @@ const Login = () => {
 
   return (
     <div className="h-[800px] flex justify-center items-center">
-      <div>
-        <h2 className="text-5xl text-center my-8">Login Here</h2>
+      <div className="w-96 p-7">
+        <h2 className="text-xl text-center my-6">Login Here</h2>
         <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
           <div className="form-control w-full max-w-xs">
             <label className="label">
-              <span className="label-text text-2xl">Email</span>
+              <span className="label-text ">Email</span>
             </label>
             <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs"/>
           </div>
           <div className="form-control w-full max-w-xs">
-            <label className="label">
-              <span className="label-text text-2xl">Password</span>
-            </label>
+            <label className="label"> <span className="label-text">Password</span></label>
             <input type="text" {...register("password")} className="input input-bordered w-full max-w-xs"/>
+            <label className="label"> <span className="label-text">Forget Password?</span></label>
           </div>
-          <p>{data}</p>
-          <input type="submit" />
+          <input className="btn btn-accent w-full" value='Login' type="submit" />
         </form>
+        <p>New to doctors portal? <Link className='text-secondary' to='/signup'>Create new account</Link></p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full font-bold">Continue With Google</button>
       </div>
     </div>
   );
