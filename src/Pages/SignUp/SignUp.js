b2eeb4d0f796824f1ec72bfca54c39350f0fa742
opuzayed <zayedopu4933@gmail.com>
2022-12-14 21:34:55 +0600

    margin added in signup button

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -31,7 +31,7 @@ const SignUp = () => {
             })} className="input input-bordered w-full max-w-xs"/>
             {errors.password && <p className='text-red-500'>{errors.password?.message}</p>}
           </div>
-          <input className="btn btn-accent w-full" value='Sign Up' type="submit" />
+          <input className="btn btn-accent w-full mt-4" value='Sign Up' type="submit" />
         </form>
         <p>Already have an account? <Link className='text-secondary' to='/login'>Please Login</Link></p>
         <div className="divider">OR</div>
