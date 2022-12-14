496b960fe5a72e6f4b8e1c3636855965a83b8b41
opuzayed <zayedopu4933@gmail.com>
2022-12-14 20:00:14 +0600

    validation password using regex

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -25,8 +25,10 @@ const SignUp = () => {
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label"> <span className="label-text">Password</span></label>
-            <input type="password" {...register("password", { required: 'Password is required',
-            minLength: { value: 6, message: "Password must be six character or long" } })} className="input input-bordered w-full max-w-xs"/>
+            <input type="password" {...register("password",{ required: 'Password is required',
+            minLength: { value: 6, message: "Password must be six character or long" },
+            pattern:{value:/(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message:'password must be strong'}
+            })} className="input input-bordered w-full max-w-xs"/>
             {errors.password && <p className='text-red-500'>{errors.password?.message}</p>}
           </div>
           <input className="btn btn-accent w-full" value='Sign Up' type="submit" />
