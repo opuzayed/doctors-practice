713c2997036c01b8cf5c15402d89579c4828d5c0
opuzayed <zayedopu4933@gmail.com>
2022-12-15 20:50:28 +0600

    signout part

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -14,6 +14,10 @@ const AuthProvider = ({children}) => {
 
     const signIn = (email,password) => {
         return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    const logOut = () => {
+        return signOut(auth);
     }
 
     useEffect(() => {
