dfbc55dfb6f7a5c4ea125246947cacf55f8e9b60
opuzayed <zayedopu4933@gmail.com>
2022-12-18 01:23:32 +0600

    navigate to home page

--- practice-dental-doctors/src/Pages/Appointment/AvailableAppoints/AvailableAppoints.js
+++ practice-dental-doctors/src/Pages/Appointment/AvailableAppoints/AvailableAppoints.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { format } from "date-fns";
 import AppointmentOption from "./AppointmentOption";
 import BookingModal from "../BookingModal/BookingModal";
--- practice-dental-doctors/src/Pages/SignUp/SignUp.js
+++ practice-dental-doctors/src/Pages/SignUp/SignUp.js
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate  } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { useContext, useState } from 'react';
+import { useContext, useState} from 'react';
 import { AuthContext } from './../../Contexts/AuthProvider';
 import { toast } from 'react-hot-toast';
 
@@ -9,6 +9,7 @@ const SignUp = () => {
     const {register, handleSubmit, formState: { errors }} = useForm();
     const {createUser,updateUser} = useContext(AuthContext);
     const [signUpError, setSignUpError] = useState('');
+    const navigate = useNavigate();
 
     const handleSignUp = data => {
         console.log(data);
@@ -22,7 +23,9 @@ const SignUp = () => {
             displayName : data.name
           }
           updateUser(userInfo)
-          .then(()=> {})
+          .then(()=> {
+            navigate('/');
+          })
           .catch(err => console.error(err));
         })
         .catch(error => {
