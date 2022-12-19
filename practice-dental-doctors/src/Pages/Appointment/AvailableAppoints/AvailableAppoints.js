dc9e7c8caeef5d9c263a8c698b6d8533a1e9f15b
opuzayed <zayedopu4933@gmail.com>
2022-12-20 00:12:07 +0600

    spinner added

--- practice-dental-doctors/src/Pages/Appointment/AvailableAppoints/AvailableAppoints.js
+++ practice-dental-doctors/src/Pages/Appointment/AvailableAppoints/AvailableAppoints.js
@@ -3,18 +3,22 @@ import { format } from "date-fns";
 import AppointmentOption from "./AppointmentOption";
 import BookingModal from "../BookingModal/BookingModal";
 import { useQuery } from "@tanstack/react-query";
+import Loading from "../../Shared/Loading/Loading";
 
 const AvailableAppoints = ({ selectedDate }) => {
  
   const [treatment, setTreatment] = useState(null);
   const date = format(selectedDate, 'PP');
-  const {data:appointmentOptions=[], refetch} = useQuery({ queryKey: ['appointmentOptions', date],
+  const {data:appointmentOptions=[], refetch, isLoading} = useQuery({ queryKey: ['appointmentOptions', date],
             queryFn: async () =>  {
               const res = await fetch(`http://localhost:5000/appointmentOptions?date=${date}`);
               const data = await res.json();
               return data;
             }});
-
+            if(isLoading)
+            {
+              return <Loading></Loading>
+            }
   return (
     <section className="my-16">
       <p className="text-center text-secondary font-bold ">
