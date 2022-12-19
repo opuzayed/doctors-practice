5cefa90978029a782853b5576b020a1031ee433b
opuzayed <zayedopu4933@gmail.com>
2022-12-20 02:50:13 +0600

    spinner added

--- practice-dental-doctors/src/Pages/Appointment/BookingModal/BookingModal.js
+++ practice-dental-doctors/src/Pages/Appointment/BookingModal/BookingModal.js
@@ -41,6 +41,9 @@ const BookingModal = ({ treatment, setTreatment, selectedDate, refetch }) => {
           toast.success("Booking confirmed");
           refetch();
         }
+        else{
+          toast.error(data.message);
+        }
       });
   };
   return (
