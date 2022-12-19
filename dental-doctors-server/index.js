219c989e1aed7b43c3f3da8a1e91dd0b7a6196ea
opuzayed <zayedopu4933@gmail.com>
2022-12-19 23:06:23 +0600

    get option and slot from specific option

--- dental-doctors-server/index.js
+++ dental-doctors-server/index.js
@@ -20,14 +20,22 @@ async function run(){
       const bookingsCollection = client.db("dentalDoctors").collection("bookings");
 
       app.get('/appointmentOptions', async(req,res)=> {
+        const date = req.query.date;
         const query = {};
         const options = await appointmentOptionCollection.find(query).toArray();
+        const bookingQuery = {appointmentDate:date};
+        const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+        options.forEach(option => {
+          const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+          const bookedSlot = optionBooked.map(book => book.slot);
+          console.log(date, option.name, bookedSlot);
+        })
+
         res.send(options);
       });
 
       app.post('/bookings', async (req, res) => {
         const booking = req.body;
-        console.log(booking);
         const result = await bookingsCollection.insertOne(booking);
         res.send(result);
       })
