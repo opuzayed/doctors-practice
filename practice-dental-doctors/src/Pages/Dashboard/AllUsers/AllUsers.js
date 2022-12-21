8a12feea923a75d2515223b8db2cff71f6136367
opuzayed <zayedopu4933@gmail.com>
2022-12-22 01:53:16 +0600

    fetch

--- practice-dental-doctors/src/Pages/Dashboard/AllUsers/AllUsers.js
+++ practice-dental-doctors/src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -1,6 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllUsers = () => {
+    const {data : users = []} = useQuery({
+        queryKey:['users'],
+        queryFn: async()=> {
+            const res = await fetch('http://localhost:5000/users');
+            const data = await res.json();
+            return data; 
+        }
+    });
     return (
         <div>
             <h2 className="text-3xl">All Users</h2>
