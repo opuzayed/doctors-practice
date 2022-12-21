a7874ad16d8786885bdb57410ca97f812b544106
opuzayed <zayedopu4933@gmail.com>
2022-12-22 02:01:25 +0600

    user show on ui

--- practice-dental-doctors/src/Pages/Dashboard/AllUsers/AllUsers.js
+++ practice-dental-doctors/src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -19,29 +19,22 @@ const AllUsers = () => {
       <tr>
         <th></th>
         <th>Name</th>
-        <th>Job</th>
+        <th>Email</th>
+        <th>Favorite Color</th>
         <th>Favorite Color</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      <tr className="hover">
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
+      {
+        users.map((user,i) =>
+        <tr key={user._id}>
+            <th>{i}</th>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>Blue</td>
+            <td>Blue</td>
+        </tr>)
+      }
     </tbody>
   </table>
 </div>
