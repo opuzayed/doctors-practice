402d68edce5fc1cb6b2fef3f405f3ca6d29b0679
opuzayed <zayedopu4933@gmail.com>
2022-12-21 19:14:58 +0600

    token install

--- dental-doctors-server/index.js
+++ dental-doctors-server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
