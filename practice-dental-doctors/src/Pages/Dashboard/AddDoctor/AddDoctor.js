94ac43e98868a05e29109fc10cab55972f8ce981
opuzayed <zayedopu4933@gmail.com>
2022-12-22 21:50:55 +0600

    add doctor

---
+++ practice-dental-doctors/src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddDoctor = () => {
+    return (
+        <div>
+           <h2 className="text-4xl">Add a Doctor</h2> 
+        </div>
+    );
+};
+
+export default AddDoctor;
--- practice-dental-doctors/src/Pages/Shared/Loading/Loading.js
+++ practice-dental-doctors/src/Pages/Shared/Loading/Loading.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div class="flex items-center justify-center">
+    <div className="flex items-center justify-center">
       <div
-        class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-green-500"
+        className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-green-500"
         role="status"
       >
-        <span class="visually-hidden">...</span>
+        <span className="visually-hidden">...</span>
       </div>
     </div>
   );
