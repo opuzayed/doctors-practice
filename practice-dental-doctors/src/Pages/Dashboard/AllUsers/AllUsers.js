09f4734af672bfa9623336a5b76d2964e92f2d58
opuzayed <zayedopu4933@gmail.com>
2022-12-22 19:26:39 +0600

    check admin

--- practice-dental-doctors/src/Pages/Dashboard/AllUsers/AllUsers.js
+++ practice-dental-doctors/src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { toast } from 'react-hot-toast';
 
 const AllUsers = () => {
-    const {data : users = []} = useQuery({
+    const {data : users = [], refetch} = useQuery({
         queryKey:['users'],
         queryFn: async()=> {
             const res = await fetch('http://localhost:5000/users');
@@ -10,6 +11,23 @@ const AllUsers = () => {
             return data; 
         }
     });
+
+    const handleMakeAdmin = id => {
+      fetch(`http://localhost:5000/users/admin/${id}`,{
+        method : 'PUT',
+        headers : {
+          authorization : `bearer ${localStorage.getItem('accessToken')}`
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        if(data.modifiedCount > 0){
+          toast.success('Make admin successful');
+          refetch();
+        }
+      });
+    }
+
     return (
         <div>
             <h2 className="text-3xl">All Users</h2>
@@ -28,10 +46,10 @@ const AllUsers = () => {
       {
         users.map((user,i) =>
         <tr key={user._id}>
-            <th>{i}</th>
+            <th>{i+1}</th>
             <td>{user.name}</td>
             <td>{user.email}</td>
-            <td><button className='btn btn-xs btn-primary'>Make Admin</button></td>
+            <td>{user?.role !== 'admin' && <button className='btn btn-xs btn-primary' onClick={()=> handleMakeAdmin(user._id)}>Make Admin</button>}</td>
             <td><button className='btn btn-xs btn-error'>Delete</button></td>
         </tr>)
       }
