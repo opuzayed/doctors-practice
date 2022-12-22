33cf16db71609e0956ccd5c76a1a7b5ef0c96134
opuzayed <zayedopu4933@gmail.com>
2022-12-22 16:49:59 +0600

    set role

--- dental-doctors-server/index.js
+++ dental-doctors-server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -103,6 +103,19 @@ async function run(){
               const result = await usersCollection.insertOne(user);
               res.send(result);
       });
+
+      app.put('/users/admin/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id : ObjectId(id)};
+        const options = { upsert: true };
+        const updateDoc = {
+      $set: {
+        role : 'admin'
+      },
+    };
+    const result = await usersCollection.updateOne( filter, updateDoc, options );
+    res.send(result);
+    });
 
     }
     finally{
