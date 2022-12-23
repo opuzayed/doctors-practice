95c633c0748e0a6f9eaf27d1667f0f03be1368cc
opuzayed <zayedopu4933@gmail.com>
2022-12-23 14:45:31 +0600

    doctors length check

--- practice-dental-doctors/src/Pages/Dashboard/Dashboard/ManageDoctors/ManageDoctors.js
+++ practice-dental-doctors/src/Pages/Dashboard/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,9 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const ManageDoctors = () => {
+    const {data : doctors} = useQuery({
+        queryKey : ['doctors'],
+        queryFn : async () => {
+            try{
+                const res = await fetch('http://localhost:5000/doctors', {
+                   headers : {
+                    authorization : `bearer ${localStorage.getItem('accessToken')}`
+                   } 
+                });
+                const data = await res.json();
+                return data;
+            }
+            catch(error){
+
+            }
+        
+        }
+        
+    })
     return (
         <div>
-            
+            <h2 className="text-3xl">Manage Doctors : {doctors?.length}</h2>
         </div>
     );
 };
