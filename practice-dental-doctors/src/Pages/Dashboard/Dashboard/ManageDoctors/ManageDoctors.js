fe944b4e3bfdf2f1b514a94d05a62b701f9f2a2f
opuzayed <zayedopu4933@gmail.com>
2022-12-23 15:18:24 +0600

    doctor table added

--- practice-dental-doctors/src/Pages/Dashboard/Dashboard/ManageDoctors/ManageDoctors.js
+++ practice-dental-doctors/src/Pages/Dashboard/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../../../Shared/Loading/Loading';
 
 const ManageDoctors = () => {
-    const {data : doctors} = useQuery({
+    const {data : doctors, isLoading} = useQuery({
         queryKey : ['doctors'],
         queryFn : async () => {
             try{
@@ -19,12 +20,47 @@ const ManageDoctors = () => {
             }
         
         }
-        
-    })
+    });
+    if(isLoading){
+        return <Loading></Loading>
+    }
     return (
         <div>
             <h2 className="text-3xl">Manage Doctors : {doctors?.length}</h2>
-        </div>
+            <div className="overflow-x-auto">
+  <table className="table w-full">  
+    <thead>
+      <tr>
+        <th></th>
+        <th>Avatar</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Specialty</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        doctors.map((doctor, i) => 
+        <tr key={doctor._id}>
+            <th>{i + 1}</th>
+            <td>
+            <div className="avatar">
+            <div className="w-24 rounded-full">
+            <img src={doctor.image} alt=''/>
+            </div>
+            </div>
+            </td>
+            <td>{doctor.name}</td>
+            <td>{doctor.email}</td>
+            <td>{doctor.specialty}</td>
+            <td><button className="btn btn-sm btn-error">Delete</button></td>
+      </tr>)
+      }
+    </tbody>
+  </table>
+</div>
+</div>
     );
 };
 
